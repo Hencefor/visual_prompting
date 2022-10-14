a0256a32d9a14207cd128bb0592c249fbcef5a5a
Hencefor <48944200+Hencefor@users.noreply.github.com>
2022-10-14 19:35:31 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -132,15 +132,15 @@ def cosine_lr(optimizer, base_lr, warmup_length, steps):
 
 
 def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
-        
         maxk = max(topk)
         batch_size = target.size(0)
+
         _, pred = output.topk(maxk, 1, True, True)
-        _, label = target.topk(maxk, 1, True, True)
         pred = pred.t()
-        label = label.t()
-        correct = pred.eq(label.expand_as(pred))
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
         res = []
         for k in topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
